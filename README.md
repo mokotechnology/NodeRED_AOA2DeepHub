757af968d406aaa07344672953dc44616c6e8821
lovexiaoxia <aadyx2007@163.com>
2024-11-14 16:41:32 +0800

    Fix

---
+++ README.md
@@ -0,0 +1,21 @@
+# Nodered-project Docker example
+
+Please use this docker-compose.yml file as a starting point for testing but keep in mind that for production usage it might need modifications.Before using this docker-compose.yml file, make sure you have Docker installed.
+
+## Configuration
+
+You can config the AOA and DEEPHUB server parameters in the docker-compose.yml.
+
+
+## Usage
+
+To start the ChirpStack simply run:
+
+```
+
+$ docker-compose up
+
+```
+
+
+
--- nodered-data/settings.js
+++ nodered-data/settings.js
@@ -13,7 +13,8 @@ module.exports = {
             port: process.env.DEEPHUB_CONFIG_PORT || 'default-port',
             token: process.env.DEEPHUB_CONFIG_TOKEN || '',
             authType: process.env.DEEPHUB_CONFIG_AUTH_TYPE || 'Bearer 3',
-            headers: JSON.parse(process.env.DEEPHUB_CONFIG_HEADERS || '{}')
+            headers: JSON.parse(process.env.DEEPHUB_CONFIG_HEADERS || '{}'),
+	    deephub_zone_source_id: process.env.DEEPHUB_ZONE_SOURCE_ID
         }
     },
     httpNodeRoot: '',
